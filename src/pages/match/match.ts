4c8f3ed9e5c75da36983e8b600864a344f06466d
giovanijfc <giovanijfc@gmail.com>
2019-03-09 16:01:10 -0300

    Quase tudo pronto, apenas algumas atualizações pra modernizar o style e alterar o back com as mensagens de alerta!

--- src/pages/match/match.ts
+++ src/pages/match/match.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, UrlSerializer } from 'ionic-angular';
 import { MenuController } from 'ionic-angular/components/app/menu-controller';
 import { MatchService } from '../../services/domain/match.service';
 import { ComparatorUtil } from '../../utilities/comparaton.util';
-import { AnonymousSubject } from 'rxjs';
 
 @IonicPage()
 @Component({
@@ -12,83 +11,126 @@ import { AnonymousSubject } from 'rxjs';
 })
 export class MatchPage {
 
-    idPartida: any;
-    match: any;
-    urls1: UrlImagesDTO;
-    urls2: UrlImagesDTO;
-    contador: any;
-   
+  idPartida: any;
+  match: any;
+  url_config: string[];
+  urls: UrlImagesDTO[] = [];
+  contador: any;
+  button1: boolean;
+  button2: boolean;
+  partidaFinalize: string;
+  soma: any;
 
-    
 
   constructor(public navCtrl: NavController,
-     public navParams: NavParams,
-     public menu: MenuController,
-     public matchService: MatchService,
-     public comparator: ComparatorUtil){
+    public navParams: NavParams,
+    public menu: MenuController,
+    public matchService: MatchService,
+    public comparator: ComparatorUtil,
+    public alertCtrl: AlertController) {
+    var a;
+    for (a = 0; a < 12; a++)this.urls[a] = { urlGrande: "", urlPequena: "" }
   }
 
   ionViewWillEnter() {
     this.menu.swipeEnable(false);
-    
+    this.partidaFinalize = "Não finalizada!";
   }
 
-  start(){
+  start() {
     this.contador = 0;
     this.matchService.start()
-     .subscribe(response =>{
-       this.match = response;
-       console.log(this.match);
-       console.log(response.dados.carUsuPar[0].cartas.naipe)
-       console.log(response.dados.carUsuPar[0].cartas.valor)
-       console.log(response.dados.carUsuPar[1].cartas.naipe)
-       console.log(response.dados.carUsuPar[1].cartas.valor)
+      .subscribe(response => {
+      
+        var c;
+        for (c = 0; c < this.urls.length; c++) { this.urls.splice(i) }
 
-       
-       
+        var a;
+        for (a = 0; a < 12; a++)this.urls[a] = { urlGrande: "", urlPequena: "" }
 
-       this.idPartida = this.match.dados.idPartida;
+        this.match = response;
+        console.log(this.match);
+        this.partidaFinalizada(response);
 
-       this.urls1 = this.comparator.compareLetter(response.dados.carUsuPar, "0");
+        this.idPartida = this.match.dados.idPartida;
 
-       this.urls2 = this.comparator.compareLetter(response.dados.carUsuPar, "1");
+        this.soma = this.match.soma;
 
-       console.log(this.idPartida);
+        this.urls[this.contador] = (this.comparator.compareLetter(response.dados.carUsuPar, this.contador));
+        ++this.contador;
+        this.urls[this.contador] = (this.comparator.compareLetter(response.dados.carUsuPar, this.contador));
 
-       console.log(this.urls1.urlGrande);
-       console.log(this.urls1.urlPequena);
+        if (this.soma == 21) {
+          this.alertNotProceed(response);
+        }
 
-       console.log(this.urls2.urlGrande);
-       console.log(this.urls2.urlPequena);
-     });
+        var i;
+        for (i = 0; i < this.urls.length; ++i) { console.log(this.urls[i]); }
+        console.log(this.soma);
+
+        ++this.contador;
+      });
   }
 
-  proceed(){
+  proceed() {
     this.matchService.proceed(this.idPartida)
-    .subscribe(response => {
-      this.match = response;
-       console.log(this.match);
-       console.log(response.dados.carUsuPar[this.contador].cartas.naipe)
-       console.log(response.dados.carUsuPar[this.contador].cartas.valor)
-
-       
-
-       this.urls1 = this.comparator.compareLetter(response.dados.carUsuPar, this.contador);
-       
-       console.log(this.idPartida);
-       console.log(this.urls1.urlGrande);
-       console.log(this.urls1.urlPequena);
-      ++this.contador;
-    })
+      .subscribe(response => {
+        this.match = response;
+        console.log(this.match);
+        this.partidaFinalizada(response);
+        this.soma = this.match.soma;
+        if (this.match.mensagem == "Esta Partida já foi finalizada!" || this.soma == 21 || this.soma > 21) {
+          if (this.match.mensagem == "Esta Partida já foi finalizada!") {
+             this.alertNotProceed(response); 
+            }
+          else if (this.soma > 21) {
+            this.urls[this.contador] = (this.comparator.compareLetter(response.dados.carUsuPar, this.contador));
+            this.alertNotProceed(response);
+          }
+          else if (this.soma == 21) {
+            this.urls[this.contador] = (this.comparator.compareLetter(response.dados.carUsuPar, this.contador));
+             this.alertNotProceed(response) 
+            }
+        } else {
+          this.urls[this.contador] = (this.comparator.compareLetter(response.dados.carUsuPar, this.contador));
+          ++this.contador;
+          var i;
+          for (i = 0; i < this.urls.length; ++i) { console.log(this.urls[i]); }
+          console.log(this.soma);
+        }
+      },
+        error => { this.alertNotProceed(this.match) })
   }
 
-  notProceed(){
+  notProceed() {
     this.matchService.notProceed(this.idPartida)
-    .subscribe(response => {
-      this.match = response;
-      console.log(this.match); 
+      .subscribe(response => {
+
+        this.partidaFinalizada(response);
+        this.match = response;
+        console.log(this.match);
+        this.alertNotProceed(response);
+      })
+  }
+
+
+  alertNotProceed(response: any) {
+    let alerta = this.alertCtrl.create({
+      title: 'Alert',
+      message: response.mensagem,
+      buttons: [{ text: 'ok', }]
     })
+    alerta.present();
+  }
+
+  partidaFinalizada(response: any) {
+    if (response.dados.partidaFinalizada != true) {
+      this.partidaFinalize = "Não finalizada!";
+    } else {
+      this.partidaFinalize = "Partida finalizada!"
+    }
   }
 }
- 
+
+
 
